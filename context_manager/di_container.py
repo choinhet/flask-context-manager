97092a3ef6cef5d3413594f9691e51cf1f9691cd
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-07-13 13:31:35 -0300

    Refactor DI container

--- context_manager/di_container.py
+++ context_manager/di_container.py
@@ -11,14 +11,17 @@ class DIContainer:
 
     @classmethod
     def start(cls):
-        DIContainer.start_components()
-        for (_, name, _) in pkgutil.iter_modules(['service']):
-            importlib.import_module(f'service.{name}')
-        DIContainer.start_services()
-        for (_, name, _) in pkgutil.iter_modules(['controller']):
-            importlib.import_module(f'controller.{name}')
-        DIContainer.start_controllers()
+        cls.start_components()
+        cls.import_modules('service')
+        cls.start_services()
+        cls.import_modules('controller')
+        cls.start_controllers()
         cls.app.run(debug=True)
+
+    @classmethod
+    def import_modules(cls, module_name):
+        for _, name, _ in pkgutil.iter_modules([module_name]):
+            importlib.import_module(f'{module_name}.{name}')
 
     @classmethod
     def register_component(cls, instance):
@@ -35,59 +38,52 @@ class DIContainer:
     @classmethod
     def start_components(cls):
         for key, component in cls.components.items():
-            instance = component()
-            cls.components[key] = instance
+            cls.components[key] = component()
 
     @classmethod
     def start_services(cls):
         for key, service in cls.services.items():
-            sig = inspect.signature(service.__init__)
-            kwargs = {}
-            for name, param in sig.parameters.items():
-                if name == 'self':
-                    continue
-                if param.annotation != inspect._empty:
-                    if param.annotation in cls.services:
-                        if param.annotation == service:
-                            raise RuntimeError(f"Circular dependency detected for service {key}")
-                        kwargs[name] = cls.services[param.annotation]
-                    elif param.annotation in cls.components:
-                        kwargs[name] = cls.components[param.annotation]
-                    else:
-                        raise RuntimeError(f"Dependency {param.annotation} not found for service {key}")
-            instance = service(**kwargs)
-            cls.services[key] = instance
+            kwargs = cls.get_constructor_arguments(service, cls.services, cls.components)
+            cls.services[key] = service(**kwargs)
 
     @classmethod
     def start_controllers(cls):
         for key, controller in cls.controllers.items():
-            sig = inspect.signature(controller.__init__)
-            kwargs = {}
-            for name, param in sig.parameters.items():
-                if name == 'self':
-                    continue
-                if param.annotation != inspect._empty:
-                    annotation = controller.__init__.__annotations__[name].decorated_class
-                    if annotation in cls.controllers:
-                        raise RuntimeError(f"Controller {key} can't depend on other controller")
-                    elif annotation in cls.services:
-                        kwargs[name] = cls.services[annotation]
-                    elif annotation in cls.components:
-                        kwargs[name] = cls.components[annotation]
-                    else:
-                        raise RuntimeError(f"Dependency {param.annotation} not found for controller {key}")
-            instance = controller(**kwargs)
-            cls.controllers[key] = instance
+            kwargs = cls.get_constructor_arguments(controller, cls.services, cls.components)
+            cls.controllers[key] = controller(**kwargs)
+            cls.register_controller_routes(cls.controllers[key])
 
-            for method_name, method in inspect.getmembers(instance, predicate=inspect.ismethod):
-                route = getattr(method, "_route", None)
-                methods = getattr(method, "_methods", None)
-                if route is not None and methods is not None:
-                    cls.app.route(route, methods=methods)(method)
+    @classmethod
+    def get_constructor_arguments(cls, obj, services, components):
+        sig = inspect.signature(obj.__init__)
+        kwargs = {}
+        for name, param in sig.parameters.items():
+            if name == 'self':
+                continue
+            if param.annotation != inspect._empty:
+                annotation = obj.__init__.__annotations__[name].decorated_class
+                if annotation in services:
+                    if param.annotation == obj:
+                        raise RuntimeError(f"Circular dependency detected for service {obj.__name__}")
+                    kwargs[name] = services[annotation]
+                elif annotation in components:
+                    kwargs[name] = components[annotation]
+                else:
+                    raise RuntimeError(f"Dependency {param.annotation} not found for object {obj.__name__}")
+        return kwargs
+
+    @classmethod
+    def register_controller_routes(cls, controller):
+        for method_name, method in inspect.getmembers(controller, predicate=inspect.ismethod):
+            route = getattr(method, "_route", None)
+            methods = getattr(method, "_methods", None)
+            if route is not None and methods is not None:
+                cls.app.route(route, methods=methods)(method)
 
     @classmethod
     def append(cls, app):
         cls.app = app
+        return cls
 
     @classmethod
     def get_app(cls):
