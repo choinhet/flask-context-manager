9d8d23cafb7b139bbc5ab37ff36bf5e8d7e380a3
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-07-13 13:57:26 -0300

    Reformat app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
 from context_manager.context_manager import ContextManager
 
 app = Flask(__name__)
+ContextManager.append(app)
 
 if __name__ == "__main__":
-    ContextManager.append(app).start()
+    ContextManager.start()
+    app.run(debug=True)
