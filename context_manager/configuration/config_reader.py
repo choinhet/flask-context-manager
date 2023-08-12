7e4a4000e824ba6a23f76f4804fddd90efb2dafe
Rafael Choinhet <rafael.choinhet@looqbox.com>
2023-08-12 00:12:52 -0300

    Fix file path

--- context_manager/configuration/config_reader.py
+++ context_manager/configuration/config_reader.py
@@ -5,7 +5,7 @@ import os
 class ConfigReader:
     _config = configparser.ConfigParser()
     _current_path = os.path.dirname(__file__)
-    _config.read(os.path.join(_current_path, "resources/config.ini"))
+    _config.read(os.path.join(_current_path, "../resources/config.ini"))
 
     @property
     def config(self):
