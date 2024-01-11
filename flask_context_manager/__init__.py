f9fdc476104ee0ea5ca59c8d378fe9d24e5ca455
Choinhet <rafael.choinhet@looqbox.com>
2024-01-11 15:10:01 -0300

    fix: import structure

--- flask_context_manager/__init__.py
+++ flask_context_manager/__init__.py
@@ -1,11 +1,11 @@
-from src.main.configuration.config_reader import *
-from src.main.core.context_manager import *
-from src.main.model.beans.component import *
-from src.main.model.beans.configuration import *
-from src.main.model.beans.controller import *
-from src.main.model.beans.repository import *
-from src.main.model.beans.service import *
-from src.main.util.key_utility import *
+from flask_context_manager.src.main.configuration.config_reader import *
+from flask_context_manager.src.main.core.context_manager import *
+from flask_context_manager.src.main.model.beans.component import *
+from flask_context_manager.src.main.model.beans.configuration import *
+from flask_context_manager.src.main.model.beans.controller import *
+from flask_context_manager.src.main.model.beans.repository import *
+from flask_context_manager.src.main.model.beans.service import *
+from flask_context_manager.src.main.util.key_utility import *
 
 __all__ = [
     "ConfigReader",
