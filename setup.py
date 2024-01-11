f28ed09acae0436c07ba6f7e3eb596629dd0f7e1
Choinhet <rafael.choinhet@looqbox.com>
2024-01-11 12:21:23 -0300

    chore: update setup

--- setup.py
+++ setup.py
@@ -1,25 +1,35 @@
+import sys
+
 from setuptools import setup, find_packages
+
+VERSION = "1.0.3"
 
 with open("README.md", "r") as fh:
     readme = fh.read()
     fh.close()
 
+if sys.version_info.major != 3 and sys.version_info.minor < 10:
+    sys.exit("'Flask Context Manager' requires Python >= 3.10!")
+
 setup(
-    name='flask-context-manager',
-    version='1.0.2',
+    name="flask-context-manager",
+    version=VERSION,
     packages=find_packages(),
-    url='https://github.com/rxonhe/flask-context-manager',
-    license='MIT',
-    author='Rafael Choinhet',
-    author_email='choinhet@gmail.com',
-    description='A lightweight dependency injection and route management system for Flask, inspired by Spring Boot.',
+    url="https://github.com/rxonhe/flask-context-manager",
+    license="MIT",
+    author="Rafael Choinhet",
+    author_email="choinhet@gmail.com",
+    description="A lightweight dependency injection and route management system for Flask, inspired by Spring Boot.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    install_requires=['flask>=3.0.0'],
+    install_requires=["flask>=3.0.0"],
+    keywords=["context", "context manager", "spring", "python", "ioc", "di", "dependency injection", "flask", "auto route"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires=">=3.10",
+    zip_safe=True,
+    include_package_data=True
 )
