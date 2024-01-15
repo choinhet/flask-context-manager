65cc9f92ffa8e9ee46e5e6aabf451ca5c9aeb66e
Choinhet <choinhet@gmail.com>
2024-01-15 09:46:57 -0300

    fix: init import

--- flask_context_manager/__init__.py
+++ flask_context_manager/__init__.py
@@ -6,11 +6,17 @@ __all__ = [
     "Controller",
     "Repository",
     "Service",
-    "KeyUtility"
+    "KeyUtility",
+    "get_mapping",
+    "post_mapping",
+    "rest_mapping",
+    "put_mapping",
+    "delete_mapping",
 ]
 
 from flask_context_manager.src.main.configuration.config_reader import ConfigReader
 from flask_context_manager.src.main.core.context_manager import ContextManager
+from flask_context_manager.src.main.core.routes import get_mapping, post_mapping, rest_mapping, put_mapping, delete_mapping
 from flask_context_manager.src.main.model.beans.component import Component
 from flask_context_manager.src.main.model.beans.configuration import Configuration
 from flask_context_manager.src.main.model.beans.controller import Controller
