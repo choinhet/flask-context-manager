db6509818e86b5f81d92c5a1106639014498621b
Choinhet <choinhet@gmail.com>
2024-01-15 09:28:43 -0300

    fix: imports

--- flask_context_manager/__init__.py
+++ flask_context_manager/__init__.py
@@ -1,13 +1,3 @@
-from flask_context_manager.src.main.configuration.config_reader import *
-from flask_context_manager.src.main.core.context_manager import *
-from flask_context_manager.src.main.core.routes import *
-from flask_context_manager.src.main.model.beans.component import *
-from flask_context_manager.src.main.model.beans.configuration import *
-from flask_context_manager.src.main.model.beans.controller import *
-from flask_context_manager.src.main.model.beans.repository import *
-from flask_context_manager.src.main.model.beans.service import *
-from flask_context_manager.src.main.util.key_utility import *
-
 __all__ = [
     "ConfigReader",
     "ContextManager",
@@ -18,3 +8,12 @@ __all__ = [
     "Service",
     "KeyUtility"
 ]
+
+from flask_context_manager.src.main.configuration.config_reader import ConfigReader
+from flask_context_manager.src.main.core.context_manager import ContextManager
+from flask_context_manager.src.main.model.beans.component import Component
+from flask_context_manager.src.main.model.beans.configuration import Configuration
+from flask_context_manager.src.main.model.beans.controller import Controller
+from flask_context_manager.src.main.model.beans.repository import Repository
+from flask_context_manager.src.main.model.beans.service import Service
+from flask_context_manager.src.main.util.key_utility import KeyUtility
